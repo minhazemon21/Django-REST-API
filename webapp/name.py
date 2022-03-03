22ad9f26599d5a68c859025b4e2b17293c3c3ff8
minhazemon21 <minhazkhondokar21@gmail.com>
2022-03-03 18:15:48 +0600

    C

--- webapp/name.py
+++ webapp/name.py
@@ -10,7 +10,7 @@
 def print_full_name(first, last):
     # Write your code here
     last = last + "!"
-    print("Hello", first, last, "You just delved into python.");
+    print("Hello", first, last, "You just delved into python.")
 
 if __name__ == '__main__':
     first_name = input()
